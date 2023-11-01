bfed5589ce8b53c261624314e9410ce87d3df69e
LUCY <wangecikariu.luc@gmail.com>
2023-11-01 20:45:30 +0300

    Finished Lab

--- lib/song.py
+++ lib/song.py
@@ -1,6 +1,7 @@
 from config import CONN, CURSOR
 
 class Song:
+    pass
     def __init__(self, name, album):
         self.id = None
         self.name = name
@@ -9,35 +10,28 @@ class Song:
     @classmethod
     def create_table(cls):
         sql = """
-            CREATE TABLE IF NOT EXISTS songs (
-                id INTEGER PRIMARY KEY,
-                name TEXT,
-                album TEXT
-            )
-        """
+                CREATE TABLE IF NOT EXISTS songs (
+                    id INTEGER PRIMARY KEY, 
+                    name TEXT, 
+                    album TEXT
+            )"""
 
         CURSOR.execute(sql)
 
-    def save(self):
-        sql = """
-            INSERT INTO songs (name, album)
-            VALUES (?, ?)
-        """
-
-        CURSOR.execute(sql, (self.name, self.album))
 
     def save(self):
         sql = """
-            INSERT INTO songs (name, album)
-            VALUES (?, ?)
-        """
+                INSERT INTO songs (name, album)
+                VALUES (?, ?)
+            """
 
         CURSOR.execute(sql, (self.name, self.album))
 
         self.id = CURSOR.execute("SELECT last_insert_rowid() FROM songs").fetchone()[0]
+        return self.id
 
     @classmethod
     def create(cls, name, album):
-        song = Song(name, album)
+        song = cls(name, album)
         song.save()
         return song
